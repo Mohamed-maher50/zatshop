0bc36ddfe124e3e9987c4e4278cee1bb214d460f
mohamedmaher <mohamedmaher.mm330@gmail.com>
2026-01-09 13:50:10 +0200

    create product api findmany and findone

--- features/products/api/queries.ts
+++ features/products/api/queries.ts
@@ -1,10 +1,15 @@
 import { baseUrl } from "@/constants/api";
-import { apiPaginatedResponse, Product } from "@/types";
+import { apiPaginatedResponse, apiResponse, Product } from "@/types";
 
 export const Products = {
   findMany: async (query: string) => {
     const productsRes = await fetch(`${baseUrl}/products${query}`);
     const res: apiPaginatedResponse<Product[]> = await productsRes.json();
+    return res;
+  },
+  findOne: async (id: string, query: string) => {
+    const productsRes = await fetch(`${baseUrl}/products/${id}/${query}`);
+    const res: apiResponse<Product> = await productsRes.json();
     return res;
   },
 };
