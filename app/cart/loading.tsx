3802cb3def9e8e77c83c78bd3db58afc28bb5aee
mohamedmaher <mohamedmaher.mm330@gmail.com>
2026-01-10 01:33:41 +0200

    create loading for cart page

--- app/cart/loading.tsx
+++ app/cart/loading.tsx
@@ -1,6 +1,13 @@
 "use client";
-const CartLoadingPage = () => {
-  return <div>loading...</div>;
+
+import { Spinner } from "@/components/ui/spinner";
+
+const CartPageSkeleton = () => {
+  return (
+    <div className="absolute flex items-center justify-center inset-0 bg-white/40  min-h-0">
+      <Spinner className="animate animate-spain size-8" />
+    </div>
+  );
 };
 
-export default CartLoadingPage;
+export default CartPageSkeleton;
